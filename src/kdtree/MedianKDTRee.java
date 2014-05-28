c09d4e9f23e10fd76a457fba052f0964b894bd52
Romina <romina-romero@git-hub.com>
2014-05-28 01:23:01 -0400

    mediankdtree, metodo para obtener splitaxis listo (falta testear...)

--- src/kdtree/MedianKDTRee.java
+++ src/kdtree/MedianKDTRee.java
@@ -5,7 +5,7 @@ public class MedianKDTRee extends KDTree{
 	@Override
 	public double getSplitAxis(double[][] puntos, int splitAxis) {
 		
-		return medianOfMedians(puntos,splitAxis);
+		return medianOfMedians(puntos,splitAxis,0,puntos.length-1);
 	}
 	/**
 	 * Aplica algoritmo de "mediana de medianas". Retorna la mediana
@@ -13,26 +13,33 @@ public class MedianKDTRee extends KDTree{
 	 * @param axis eje
 	 * @return mediana
 	 */
-	private double medianOfMedians(double[][] puntos, int axis){
-		switch (puntos.length){
+	private double medianOfMedians(double[][] puntos, int axis,int start,int end){
+		switch (end-start){
 		case 1: return puntos[0][axis];
 		case 2: return (puntos[0][axis]+puntos[1][axis])/2;
-		case 3: return medianOfThree(puntos, axis);
-		case 4: return medianOfFour(puntos, axis);
-		case 5: return medianOfFive(puntos,axis);
-		default: return 0;
-			//TODO
+		case 3: return medianOfThree(puntos, axis,start);
+		case 4: return medianOfFour(puntos, axis,start);
+		case 5: return medianOfFive(puntos,axis,start);
+		default:
+			{
+				double [][] medians=new double[puntos.length/5+(puntos.length%5==0 ? 0 : 1)][2];
+				
+				int i=0;
+				
+				for(int j=start; j<=end; j+=5){
+
+					medians[i++][axis]=medianOfMedians(puntos, axis,j,j+4);
+					
+				}
+				
+				return medianOfMedians(medians, axis,0,medians.length);
+			}
 		}
 
 	}
 	
-	private double[][][] splitMaxLength(double[][] puntos, int maxLength){
-		//TODO
-		return new double[1][1][1];
-	}
-	
 
-	private double medianOfThree(double[][] puntos, int axis){
+	private double medianOfThree(double[][] puntos, int axis, int start){
 		if(puntos[0][axis] > puntos[2][axis])
 			swap(puntos,0,2);
 		
@@ -45,7 +52,7 @@ public class MedianKDTRee extends KDTree{
 		return puntos[1][axis];
 	}
 	
-	private double medianOfFour(double[][] puntos, int axis){
+	private double medianOfFour(double[][] puntos, int axis, int start){
 		
 		if(puntos[0][axis]>puntos[1][axis])
 			swap(puntos,0,1);
@@ -63,7 +70,7 @@ public class MedianKDTRee extends KDTree{
 		
 	}
 	
-	private double medianOfFive(double[][] puntos, int axis){
+	private double medianOfFive(double[][] puntos, int axis, int start){
 
 		if(puntos[0][axis]>puntos[1][axis])
 			swap(puntos, 0,1);
@@ -71,22 +78,25 @@ public class MedianKDTRee extends KDTree{
 		if(puntos[2][axis]>puntos[3][axis])
 			swap(puntos,2,3);
 		
-		//dejar en indice 0 el menor de los 4 (que no es mediana)
+		//dejar en indice 0 el menor de los 4 (no es mediana)
 		if(puntos[0][axis] > puntos[2][axis])
 			swap(puntos,0,2);
 		
-		//dejar en indice 4 el mayor de los 4
+		//dejar en indice 3 el mayor de los 4 (no es mediana)
+		if(puntos[1][axis] > puntos[3][axis])
+			swap(puntos,1,3);
+		
 		if(puntos[2][axis] > puntos[4][axis])
-			swap(puntos,2,4);
+			swap(puntos,1,3);
+				
+		//dejar en indice 1 el menor de de los segundos 4 (no es mediana)
+		if(puntos[1][axis] > puntos[2][axis])
+			swap(puntos,1,2);
+
+		//como en el indice 2 se dejo un elemento que es menor que otros 2, 
+		//y no es ni el primero ni segundo elemento menor, es mediana
 		
-		//encontrar mediana de 3
-		double[][] others=new double[3][2];
-		others[0]=puntos[1];
-		others[1]=puntos[2];
-		others[2]=puntos[4];
-		
-		return medianOfThree(others,axis);
-		
+		return puntos[2][axis];
 	}
 	
 	/**
