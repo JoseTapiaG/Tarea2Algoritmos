eed0607c06328f717d2dfa1d97c6aa6f0255c531
jose <jose.wt@gmail.com>
2014-05-31 13:25:27 -0400

    Se arma metodo para encontrar el vecino mas cercano. Se encuentra area a la que pertenece q. Falta revisar si hay mejores soluciones, en particular falta ver si el area intersecta el circulo con origen en q y radio distActual

--- src/main/VecinoMasCercano.java
+++ src/main/VecinoMasCercano.java
@@ -6,25 +6,74 @@ import kdtree.MainKDTRee;
 import node.Node;
 
 public class VecinoMasCercano {
+	static public double[] mejorActual;
+	static public double distActual;
 
 	static public void main(String[] args) {
 		MainKDTRee mainKDTree = new MainKDTRee();
 		ArrayList<double[]> puntos = new ArrayList<double[]>();
 		for (int i = 0; i < 10; i++) {
-			double[] punto = {i, i};
+			double[] punto = { i, i };
 			puntos.add(punto);
 		}
 		Node root = mainKDTree.construirKDTree(puntos, 0);
+		mejorActual = new double[2];
+		mejorActual[0] = 0;
+		mejorActual[1] = 0;
+		distActual = Double.MAX_VALUE;
 		System.out.println("hola");
 	}
 
-	public int[] vecinoMasCercano(Node root, int[] q) {
-
-		// recorrer arbol como si se fuese a insertar el punto
-		// se identifica H cuya region tiene a q
-		// mejor actual p y hoja H
-		// distancia actual entre p y q
-		return new int[2];
+	private double[] vecinoMasCercano(Node root, double[] q) {
+		Node nodoDondeEstaQ = encontrarHojaDondeEstaQ(root, q);
+		buscarMejorSolucion(nodoDondeEstaQ, q);
+		return mejorActual;
 	}
 
+	private Node encontrarHojaDondeEstaQ(Node node, double[] q) {
+		node.setVisitado(true);
+		if (node.getLeft() == null || node.getRight() == null) {
+			double distNueva = Math.sqrt((Math.pow((q[0] - node.getValue()[0]),
+					2) + Math.pow((q[1] - node.getValue()[1]), 2)));
+			distActual = distNueva;
+			mejorActual = node.getValue();
+			return node;
+		} else {
+			if ((node.getValue()[0] == 0 && q[1] > node.getValue()[1])
+					|| (node.getValue()[1] == 0 && q[0] > node.getValue()[0])) {
+				return encontrarHojaDondeEstaQ(node.getRight(), q);
+			} else {
+				return encontrarHojaDondeEstaQ(node.getLeft(), q);
+			}
+		}
+	}
+
+	private void buscarMejorSolucion(Node node, double[] q) {
+		if (node.getPadre() == null) {
+			return;
+		}
+
+		// es hoja
+		if (node.getLeft() == null && node.getRight() == null) {
+			double distNueva = Math.sqrt((Math.pow((q[0] - node.getValue()[0]),
+					2) + Math.pow((q[1] - node.getValue()[1]), 2)));
+			if (distNueva < distActual) {
+				distActual = distNueva;
+				mejorActual = node.getValue();
+			}
+		} else {
+			if (!node.getLeft().isVisitado() && areaIntersecta(node, q, true)) {
+				buscarMejorSolucion(node.getLeft(), q);
+			} else if (!node.getRight().isVisitado()
+					&& areaIntersecta(node, q, false)) {
+				buscarMejorSolucion(node.getRight(), q);
+			}
+		}
+		buscarMejorSolucion(node.getPadre(), q);
+
+	}
+
+	private boolean areaIntersecta(Node node, double[] q, boolean izq) {
+		return false;
+	}
 }
--- src/node/Node.java
+++ src/node/Node.java
@@ -4,12 +4,14 @@ public class Node {
 
 	private double[] value;
 	private Node left, right, padre;
-
+	private boolean visitado;
+	
 	public Node(double[] value, Node padre, Node left, Node right) {
 		this.value = value;
 		this.padre = padre;
 		this.right = right;
 		this.left = left;
+		visitado = false;
 	}
 
 	public double[] getValue() {
@@ -31,4 +33,14 @@ public class Node {
 	public void setPadre(Node padre) {
 		this.padre = padre;
 	}
+
+	public boolean isVisitado() {
+		return visitado;
+	}
+
+	public void setVisitado(boolean visitado) {
+		this.visitado = visitado;
+	}
+	
+	
 }
