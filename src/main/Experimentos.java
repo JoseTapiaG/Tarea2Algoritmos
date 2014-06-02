60d0b071d5ba3f0c2ab0ca4f4ac102787730b307
Romina <romina-romero@git-hub.com>
2014-06-02 01:13:51 -0400

    agregado avance en metodos de experimentos

--- src/main/Experimentos.java
+++ src/main/Experimentos.java
@@ -2,6 +2,7 @@ package main;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Random;
 
 import kdtree.*;
 
@@ -10,6 +11,8 @@ public class Experimentos {
 	private KDTree mainKDTree;
 	private KDTree medianKDTree;
 	private PrintWriter printWriter;
+	private double c;
+	private Random randomGenerator;
 	
 	
 	static public void main(String[]args){
@@ -26,33 +29,76 @@ public class Experimentos {
 		mainKDTree = new MainKDTRee();
 		medianKDTree= new MedianKDTRee();
 		this.printWriter=printwriter;
+		randomGenerator = new Random();
 		
 	}
 	
 	public Experimentos(){
 		
-		mainKDTree = new MainKDTRee();
-		medianKDTree= new MedianKDTRee();
-		this.printWriter=new PrintWriter(System.out); //en pantalla
+		this(new PrintWriter(System.out));
 		
 	}
 	
 	
 	public void testMemoriaPrincipal(){
-	
 		
+		for(int n=10; n<=20; n++){
+			
+			testPuntosAleatorios((int)Math.pow(2, n),mainKDTree);
+			testPuntosAleatorios((int)Math.pow(2, n),medianKDTree);
+			testPuntosPocaDisc((int)Math.pow(2, n),mainKDTree);
+			testPuntosPocaDisc((int)Math.pow(2, n),medianKDTree);
+			
+			
+		}
 		
 	}
 	
 	public void testMemoriaSecundaria(){
 		
+		//TODO
 		
+	}
+
+	private void testPuntosAleatorios(int n, KDTree k){
+		//TODO
 		
+	}
+	
+	private void testPuntosPocaDisc(int n, KDTree k){
+		//TODO
+		
+	}
+	
+	private double mean(ArrayList<Double> list){
+		double val=0;
+		for (double v : list){
+			val+=v;
+		}
+		return val/list.size();
+	}
+	
+	private double std(ArrayList<Double> list){
+		double m=mean(list);
+		double val=0;
+		for(double v : list){
+			val+=Math.pow(v-m,2);
+		}
+		
+		return Math.sqrt(val/list.size());
 	}
 	
 	private ArrayList<double[]> generarPuntosAleatorios(int n){
 		
-		return null;
+		c=randomGenerator.nextDouble();
+		
+		ArrayList<double[]> points=new ArrayList<double[]>();
+		
+		for(int i=0; i<n; i++){
+			double[] newPoint={randomGenerator.nextDouble()*Math.sqrt(n)*c,randomGenerator.nextDouble()*Math.sqrt(n)*c};
+			points.add(newPoint);
+		}
+		return points;
 		
 	}
 	
